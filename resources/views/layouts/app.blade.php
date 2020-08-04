7cd75272d751a8468438c6182df5c3269f6abc48
Nitu <dilara.nitu@gmail.com>
2020-08-04 15:23:55 +0600

    updated Index Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@
       <div class="row mb-5">
         <div class="col-md">
           <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">dirEngine</h2>
+            <h2 class="ftco-heading-2">HappyTours</h2>
             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
             <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
               <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
@@ -122,7 +122,7 @@
           	<h2 class="ftco-heading-2">Have a Questions?</h2>
           	<div class="block-23 mb-3">
               <ul>
-                <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                <li><span class="icon icon-map-marker"></span><span class="text">Daffodil International University,Dhanmondi,Dhaka,Bangladesh.</span></li>
                 <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
                 <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
               </ul>
