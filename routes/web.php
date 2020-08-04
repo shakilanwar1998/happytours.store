40b1f116b0a023b8dcfd36e5d7d447671272936c
Nitu <dilara.nitu@gmail.com>
2020-08-04 15:56:02 +0600

    place layout-1

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function (){
     return view('home');
 });
+Route::get('/place', function (){
+    return view('layouts.places');
+});
+
 
 Auth::routes();
 
