02901d3bfdbabddf89ad2b17baf5386848db14af
Nitu <dilara.nitu@gmail.com>
2020-08-05 23:16:15 +0600

    place layout added

--- resources/views/layouts/places.blade.php
+++ resources/views/layouts/places.blade.php
@@ -3,7 +3,7 @@
 
 
 <head>
-    <title>DirEngine - Free Bootstrap 4 Template by Colorlib</title>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
@@ -434,7 +434,7 @@
   
 
   <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  <!-- <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div> -->
 
 
   <script src="js/jquery.min.js"></script>
@@ -456,7 +456,7 @@
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
     
-  </body>
+
 
 
 
